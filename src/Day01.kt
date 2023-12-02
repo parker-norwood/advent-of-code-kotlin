f87d8f775b8143772c8ba5077aa262dd70346bee
Parker Norwood <24658209+parker-norwood@users.noreply.github.com>
2023-12-02 15:37:52 -0500

    Advent of Code Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,52 @@
+val NumbersArray = arrayOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+
+val NumbersMap = mapOf(
+    "1" to "1",
+    "2" to "2",
+    "3" to "3",
+    "4" to "4",
+    "5" to "5",
+    "6" to "6",
+    "7" to "7",
+    "8" to "8",
+    "9" to "9",
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9"
+)
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    print("Part 1: ")
+    readInput("Day01")
+        .sumOf { line ->
+            val (first) = NumbersArray
+                .map { it to line.indexOf(it) }
+                .minBy { (_, value) -> if (value < 0) Int.MAX_VALUE else value }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+            val (last) = NumbersArray
+                .map { it to line.lastIndexOf(it) }
+                .maxBy { (_, value) -> if (value < 0) Int.MIN_VALUE else value }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+            (first + last).toInt()
+        }.println()
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    print("Part 2: ")
+    readInput("Day01")
+        .sumOf { line ->
+            val (first) = NumbersMap.keys
+                .map { it to line.indexOf(it) }
+                .minBy { (_, value) -> if (value < 0) Int.MAX_VALUE else value }
+
+            val (last) = NumbersMap.keys
+                .map { it to line.lastIndexOf(it) }
+                .maxBy { (_, value) -> if (value < 0) Int.MIN_VALUE else value }
+
+            (NumbersMap[first] + NumbersMap[last]).toInt()
+        }.println()
 }
